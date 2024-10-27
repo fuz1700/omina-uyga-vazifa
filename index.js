6fe8504f51b8ea1fcdf64ec8c4f9a700b4aa2f1f
fuz1700 <fuz77it@gmail.com>
2024-10-27 16:32:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let boyi = 1.85;
+let vesi = 85;
+let bmi = vesi / (boyi*boyi);
+console.log(bmi);
+
+
+let habar = prompt("CSB balingizni kiriting!")
+
+if(habar <="40"){
+    alert(" KIROMISIZ❌❌!")
+}else if( habar >= '40 ball' && habar <= '100'){
+    alert("TABRIKLAYMIZ✅✅!")
+}else if  (  habar >=40  && habar<=100){
+    alert("PUL TOLAYSIZ!")
+}else if ( habar>40){
+    alert(" OLQIWLAYMIZ PUL TOLAMAYSIZ!")
+
+}
+
+
